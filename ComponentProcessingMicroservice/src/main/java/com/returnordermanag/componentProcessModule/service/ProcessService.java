44225cdc6833d2f0b9efd741b9995839f31a0670
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-15 05:20:09 +0530

    Delete ComponentProcessingMicroservice directory

--- ComponentProcessingMicroservice/src/main/java/com/returnordermanag/componentProcessModule/service/ProcessService.java
+++
@@ -1,7 +0,0 @@
-package com.returnordermanag.componentProcessModule.service;
-
-import com.returnordermanag.componentProcessModule.model.ProcessResponse;
-
-public interface ProcessService {
-	ProcessResponse processDetail(int userID);
-}
--- ComponentProcessingMicroservice/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into process_request values (1, 'Laptop', 'integral', 1234567890, 9087654321, true, 1, 'Lakshit');
-insert into process_request values (2, 'Charger', 'accessory', 1234567890, 9087654321, true, 2, 'Ananya');
-insert into process_request values (3, 'Mobile', 'integral', 1234567890, 9087654321, false, 3, 'Arya');
-insert into process_request values (4, 'Earphones', 'accessory', 1234567890, 9087654321, false, 1, 'Vishwajeet');
